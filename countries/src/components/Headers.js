eef4f6c69904e7681defbeab0900086b26c6a886
sarah loghmani <sarah.loghmani80@gmail.com>
2022-06-08 20:04:40 +0430

    2

---
+++ countries/src/components/Headers.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Headers() {
+  return (
+    <>
+      <header className="header">
+        <div>
+            <h1>Choose your destination</h1>
+        </div>
+
+        <div>
+            <i className="fas fa-moon"></i>Dark Mood
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Headers;
