edcbf780015b280f9e1ca1994191ba18bbbb376f
sarah loghmani <sarah.loghmani80@gmail.com>
2022-06-08 20:04:35 +0430

    1

--- countries/src/App.js
+++ countries/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Countries from "./components/Countries";
+import Headers from "./components/Headers";
 
 function App() {
   return (
     <>
+      <Headers/>
       <Countries />
     </>
   );
