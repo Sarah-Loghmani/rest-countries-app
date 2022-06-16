96689aa6d075a686e1e8d40b30368de83b2be3af
sarah loghmani <sarah.loghmani80@gmail.com>
2022-06-16 23:03:16 +0430

    4

--- countries/src/components/Headers.js
+++ countries/src/components/Headers.js
@@ -5,32 +5,40 @@ function Headers() {
     const sun = document.querySelector(".dark-mood");
 
     const i = document.querySelector(".fa-sun");
+    // console.log(i.classList);
     // const iSpan = document.querySelector(".dark-mood span");
     // console.log(iSpan);
+
+    const cards = document.querySelectorAll(".grid article");
 
     const header = document.querySelector(".header");
     const input = document.querySelector(".form input");
     const regionFilter = document.querySelector(
       ".filter .region-filter select"
     );
-    const regionOptions = document.querySelectorAll(
-      ".filter .region-filter select option"
-    );
+    const regionOptions = document.getElementsByName("option");
     const cardsDetails = document.querySelectorAll(".grid .details");
 
     // console.log(sun);
     sun.addEventListener("click", () => {
-      i.classList.toggle("fa-sun");
+      i.classList.value === "fas fa-sun"
+        ? (i.classList.value = "fas fa-moon")
+        : (i.classList.value = "fas fa-sun");
       // iSpan.classList.toggle("lightSpan")
       // iSpan.classList.toggle("darkSpan");
       document.body.classList.toggle("light-mood");
       header.classList.toggle("light-mood");
       input.classList.toggle("light-mood");
       regionFilter.classList.toggle("light-mood");
-      regionOptions.classList.toggle("light-mood");
+      regionOptions.forEach((option) => {
+        option.classList.toggle("light-mood");
+      });
       cardsDetails.forEach((detail) => {
         detail.classList.toggle("light-mood");
       });
+       cards.forEach((card) => {
+         card.classList.toggle("light-mood");
+       });
     });
   };
   return (
