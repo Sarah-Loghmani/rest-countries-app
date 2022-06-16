02c89e9d0c83f09a4548af2649c1fdb6519c7780
sarah loghmani <sarah.loghmani80@gmail.com>
2022-06-16 20:59:47 +0430

    4

--- countries/src/components/Headers.js
+++ countries/src/components/Headers.js
@@ -3,9 +3,36 @@ import React from "react";
 function Headers() {
   const handleClick = () => {
     const sun = document.querySelector(".dark-mood");
-    console.log(sun);
+
+    const i = document.querySelector(".fa-sun");
+    // const iSpan = document.querySelector(".dark-mood span");
+    // console.log(iSpan);
+
+    const header = document.querySelector(".header");
+    const input = document.querySelector(".form input");
+    const regionFilter = document.querySelector(
+      ".filter .region-filter select"
+    );
+    const regionOptions = document.querySelectorAll(
+      ".filter .region-filter select option"
+    );
+    const cardsDetails = document.querySelectorAll(".grid .details");
+
+    // console.log(sun);
     sun.addEventListener("click", () => {
+      i.classList.toggle("fa-sun");
+      i.classList.toggle("fa-moon");
+
+      // iSpan.classList.toggle("lightSpan")
+      // iSpan.classList.toggle("darkSpan");
       document.body.classList.toggle("light-mood");
+      header.classList.toggle("light-mood");
+      input.classList.toggle("light-mood");
+      regionFilter.classList.toggle("light-mood");
+      regionOptions.classList.toggle("light-mood");
+      cardsDetails.forEach((detail) => {
+        detail.classList.toggle("light-mood");
+      });
     });
   };
   return (
@@ -17,8 +44,8 @@ function Headers() {
 
         <div className="dark-mood" onClick={() => handleClick()}>
           <i className="fas fa-sun"></i>
-          {/* <i className="fas fa-moon"></i> */}
-          <span>Light Mood</span>
+          {/* <span className="">Light Mood</span>
+          <span className="darkSpan">Dark Mood</span> */}
         </div>
       </header>
     </>
