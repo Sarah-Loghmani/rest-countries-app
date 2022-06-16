a02792b2f0ec07e34720b69e7cc0b2f0bd8de6d6
sarah loghmani <sarah.loghmani80@gmail.com>
2022-06-16 20:59:42 +0430

    3

--- countries/src/components/Filter.js
+++ countries/src/components/Filter.js
@@ -1,32 +1,32 @@
-import React from 'react';
+import React from "react";
 
 function Filter() {
   return (
     <div>
       <section className="filter">
-        <form className="form-control">
+        <form className="form">
           <input
             type="search"
             name="search"
             id="search"
-            placeholder= "Search for a country"
+            placeholder="Search for a country"
             autoComplete="off"
           />
         </form>
 
         <div className="region-filter">
-        <select name="select" id="select" className="select">
-          <option value="Filter by region">Filter by region</option>
-          <option value="Africa">Africa</option>
-          <option value="America">America</option>
-          <option value="Asia">Asia</option>
-          <option value="Europe">Europe</option>
-          <option value="Oceania">Oceania</option>
-        </select>
+          <select name="select" id="select" className="select">
+            <option value="Filter by region">Filter by region</option>
+            <option value="Africa">Africa</option>
+            <option value="America">America</option>
+            <option value="Asia">Asia</option>
+            <option value="Europe">Europe</option>
+            <option value="Oceania">Oceania</option>
+          </select>
         </div>
       </section>
     </div>
   );
 }
 
-export default Filter   
+export default Filter;
