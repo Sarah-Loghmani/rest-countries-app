cd1243de9b7a0cb7964c3736e053c0e99c96d575
sarah loghmani <sarah.loghmani80@gmail.com>
2022-06-20 21:07:21 +0430

    1

--- countries/src/components/Headers.js
+++ countries/src/components/Headers.js
@@ -4,39 +4,38 @@ function Headers() {
   const handleClick = () => {
     const sun = document.querySelector(".dark-mood");
 
-    const i = document.querySelector(".fa-sun");
-    // console.log(i.classList);
+    // const i = document.querySelector(".header .fa-sun");
+    // console.log(i);
     // const iSpan = document.querySelector(".dark-mood span");
     // console.log(iSpan);
 
     const cards = document.querySelectorAll(".grid article");
 
     const header = document.querySelector(".header");
-    const input = document.querySelector(".form input");
-    const regionFilter = document.querySelector(
-      ".filter .region-filter select"
-    );
-    const regionOptions = document.querySelectorAll("option");
+    // const input = document.querySelector("input");
+    // console.log(input);
+    // const regionFilter = document.querySelector(
+    //   ".filter .region-filter select"
+    // );
+    // const regionOptions = document.querySelectorAll("option");
     const cardsDetails = document.querySelectorAll(".grid .details");
     // country
-    const borders = document.querySelectorAll(
-      ".country article .borders ul li"
-    );
+    const borders = document.querySelectorAll(".country article .borders ul li");
 
     // console.log(sun);
     sun.addEventListener("click", () => {
-      i.classList.value !== "fas fa-sun"
-        ? (i.classList.value = "fas fa-sun")
-        : (i.classList.value = "fas fa-moon");
+      // i.classList.toggle("fa-sun") &&
+      // i.classList.toggle("fa-moon");
+
       // iSpan.classList.toggle("lightSpan")
       // iSpan.classList.toggle("darkSpan");
       document.body.classList.toggle("light-mood");
       header.classList.toggle("light-mood");
-      input.classList.toggle("light-mood");
-      regionFilter.classList.toggle("light-mood");
-      regionOptions.forEach((option) => {
-        option.classList.toggle("light-mood");
-      });
+      // input.classList.toggle("light-mood");
+      // regionFilter.classList.toggle("light-mood");
+      // regionOptions.forEach((option) => {
+      //   option.classList.toggle("light-mood");
+      // });
       cardsDetails.forEach((detail) => {
         detail.classList.toggle("light-mood");
       });
@@ -57,6 +56,7 @@ function Headers() {
         </div>
 
         <div className="dark-mood" onClick={() => handleClick()}>
+          <i className="fas fa-moon"></i>
           <i className="fas fa-sun"></i>
           {/* <span className="">Light Mood</span>
           <span className="darkSpan">Dark Mood</span> */}
